8eb78f8677b4a3db76441f03859915a6586c9d7f
M. Nurul Abser <absern46@gmail.com>
2022-09-29 18:45:49 +0600

    first commit

---
+++ js/mart.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('buySome').addEventListener('click', function () {
+    window.location.href = "rolex.html";
+})
+
+document.getElementById('buySome1').addEventListener('click', function () {
+    window.location.href = "torgeon.html";
+})
